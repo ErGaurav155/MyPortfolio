02f201719965ef63d53ce43f77b1c604135aa049
Guarav Khaire <gauravgkhaire@gmail.com>
2025-05-02 19:06:58 +0530

    now

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,4 +1,3 @@
-import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
@@ -13,25 +12,6 @@ const transporter = nodemailer.createTransport({
     pass: process.env.GMAIL_PASSKEY,
   },
 });
-
-// Helper function to send a message via Telegram
-async function sendTelegramMessage(
-  token: string,
-  chat_id: string,
-  message: string
-) {
-  const url = `https://api.telegram.org/bot${token}/sendMessage`;
-  try {
-    const res = await axios.post(url, {
-      text: message,
-      chat_id,
-    });
-    return res.data.ok;
-  } catch (error) {
-    console.error("Error sending Telegram message:", error);
-    return false;
-  }
-}
 
 // HTML email template
 const generateEmailTemplate = (
@@ -84,29 +64,15 @@ export async function POST(request: NextRequest) {
   try {
     const payload = await request.json();
     const { name, email, message: userMessage } = payload;
-    const token = process.env.TELEGRAM_BOT_TOKEN;
-    const chat_id = process.env.TELEGRAM_CHAT_ID;
 
     // Validate environment variables
-    if (!token || !chat_id) {
-      return NextResponse.json(
-        {
-          success: false,
-          message: "Telegram token or chat ID is missing.",
-        },
-        { status: 400 }
-      );
-    }
 
     const message = `New message from ${name}\n\nEmail: ${email}\n\nMessage:\n\n${userMessage}\n\n`;
-
-    // Send Telegram message
-    const telegramSuccess = await sendTelegramMessage(token, chat_id, message);
 
     // Send email
     const emailSuccess = await sendEmail(payload, message);
 
-    if (telegramSuccess && emailSuccess) {
+    if (emailSuccess) {
       return NextResponse.json(
         {
           success: true,
