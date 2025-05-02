7d3e93b3d2209defb81a8ff7b411db8e930a0593
Guarav Khaire <gauravgkhaire@gmail.com>
2025-05-02 18:55:45 +0530

    now

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="flex items-center justify-center w-full text-xl font-normal">
+      ...Loading
+    </div>
+  );
+};
+
+export default loading;
