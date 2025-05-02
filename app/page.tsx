041e34473547a286203cfd202c448b7d7bdbf264
Guarav Khaire <gauravgkhaire@gmail.com>
2025-05-02 18:24:48 +0530

    now

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,20 @@ import Experience from "@/components/homepage/experience/Experience";
 import HeroSection from "@/components/homepage/hero-section/HeroSec";
 import Projects from "@/components/homepage/projects/Projects";
 import Skills from "@/components/homepage/skills/Skills";
+import { Suspense } from "react";
 
 export default async function Home() {
   return (
     <div className="flex flex-col gap-10">
-      <HeroSection />
-      <AboutSection />
-      <Experience />
-      <Skills />
-      <Projects />
-      <Education />
-      <ContactSection />
+      <Suspense fallback={<>Loading...</>}>
+        <HeroSection />
+        <AboutSection />
+        <Experience />
+        <Skills />
+        <Projects />
+        <Education />
+        <ContactSection />
+      </Suspense>
     </div>
   );
 }
