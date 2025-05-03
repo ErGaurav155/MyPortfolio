0dbbc6d2cadabb08f99c9b1833497b64a4f969c5
Guarav Khaire <gauravgkhaire@gmail.com>
2025-05-03 12:33:07 +0530

    abb

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: "https",
-  //       hostname: "ik.imagekit.io",
-  //       port: "",
-  //     },
-  //   ],
-  // },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
