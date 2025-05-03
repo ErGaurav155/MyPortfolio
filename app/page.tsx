d4d5d35666e18a68744ec017861a6b56fbfda293
Guarav Khaire <gauravgkhaire@gmail.com>
2025-05-03 14:20:58 +0530

    abb

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,23 @@
 // import Skills from "@/components/homepage/skills/Skills";
 
 import AboutSection from "@/components/homepage/about/About";
-import Experience from "@/components/homepage/experience/Experience";
+import ContactSection from "@/components/homepage/contact/Contact";
+import Education from "@/components/homepage/education/Education";
+// import Experience from "@/components/homepage/experience/Experience";
 import HeroSection from "@/components/homepage/hero-section/HeroSec";
+import Projects from "@/components/homepage/projects/Projects";
+import Skills from "@/components/homepage/skills/Skills";
 
 export default async function Home() {
   return (
     <div className="flex flex-col items-center justify-center gap-10">
       <HeroSection />
       <AboutSection />
-      <Experience />
-      {/* <Skills />
+      {/* <Experience /> */}
+      <Skills />
       <Projects />
       <Education />
-      <ContactSection /> */}
+      <ContactSection />
     </div>
   );
 }
