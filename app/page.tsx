b57b5ccadce4dcb34e364acaf02ffc282cb8d477
Guarav Khaire <gauravgkhaire@gmail.com>
2025-05-03 14:08:02 +0530

    now

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-import AboutSection from "@/components/homepage/about/About";
-import ContactSection from "@/components/homepage/contact/Contact";
-import Education from "@/components/homepage/education/Education";
-import Experience from "@/components/homepage/experience/Experience";
-import HeroSection from "@/components/homepage/hero-section/HeroSec";
-import Projects from "@/components/homepage/projects/Projects";
-import Skills from "@/components/homepage/skills/Skills";
-
 export default async function Home() {
   return (
     <div className="flex flex-col items-center justify-center gap-10">
