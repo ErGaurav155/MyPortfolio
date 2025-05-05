3bc1c5861920b877922814bb9dfa14eababc4ac5
Guarav Khaire <gauravgkhaire@gmail.com>
2025-05-05 18:25:43 +0530

    now

--- utils/data/personal-data.ts
+++ utils/data/personal-data.ts
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Gaurav Khaire",
-  profile: "/profile1.jpg",
+  profile: "/profile.jpeg",
   designation: "Full Stack Nextjs Developer",
   description:
     "Gaurav Khaire is a Mumbai-based full-stack Next.js developer and solopreneur who founded an Ainspiretech AI agency and CriconAi SaaS platform offering cutting-edge AI tools. With a strong foundation in modern web technologies, he combines technical expertise and product thinking to build intelligent, scalable digital solutions. Alongside his entrepreneurial journey, Gaurav has also worked as a freelancer, helping businesses bring their ideas to life through clean code, automation, and AI integration",
